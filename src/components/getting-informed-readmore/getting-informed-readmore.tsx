78d36496e8a761d5f26470c7fbe4943ba5d8a13d
Judith Lippold <judithlippold@gmx.net>
2024-05-22 22:57:16 +0200

    style getting informed read more cards according to new design
    also added some global styles

--- src/components/getting-informed-readmore/getting-informed-readmore.tsx
+++ src/components/getting-informed-readmore/getting-informed-readmore.tsx
@@ -13,45 +13,52 @@ import { getTranslation } from '@/localization/i18n'
 import styles from './page.module.css'
 import copyInformed from '@/localization/getting-informed/en.json'
 
-const GettingInformedReadMore = async ({ lang, id }: { lang: string, id: string }) => {
-  const { t } = await getTranslation(lang,  'getting-informed', {
+const GettingInformedReadMore = async ({
+  lang,
+  id,
+}: {
+  lang: string
+  id: string
+}) => {
+  const { t } = await getTranslation(lang, 'getting-informed', {
     keyPrefix: 'getting-informed',
   })
 
   const filterArticle = (_section: object, index: number) => {
-    return index+1 !== parseInt(id)
+    return index + 1 !== parseInt(id)
   }
 
   return (
     <Container fluid className={styles.sectionOrange}>
       <div className={styles.text}>
         <h1 className={styles.heading1}> {t('title')}</h1>
-        <p className={styles.paragraph}>{t('description')}</p>
+        <p className={"sectionIntro"}>{t('description')}</p>
       </div>
       <div className={styles.subsection}>
-        <Row className={`g-1 row-gap-4`} xs={1}>
-          {copyInformed['getting-informed'].sections.map((_section, index) => (
-            <Container key={index}>
-              <Link
-                className={styles.myCardTitle}
-                href={t(`sections.${index}.link.href`)}
-              >
-                <Card bsPrefix="myCard" className={styles.myCard}>
-                  <CardImg
-                    variant="top"
-                    src={t(`sections.${index}.image.src`)}
-                    alt={t(`sections.${index}.image.alt`)}
-                    width={100}
-                  />
-                  <CardBody>
-                    <CardTitle className={styles.myCardTitle}>
-                      {t(`sections.${index}.title`)}
-                    </CardTitle>
-                  </CardBody>
-                </Card>
-              </Link>
-            </Container>
-          )).filter(filterArticle)}
+        <Row className={`row-gap-4 gx-1 gy-1`} xs={1} md={2}>
+          {copyInformed['getting-informed'].sections
+            .map((_section, index) => (
+              <Container key={index}>
+                <Link
+                  className={styles.myCardTitle}
+                  href={t(`sections.${index}.link.href`)}
+                >
+                  <Card bsPrefix="myCard" className={styles.myCard}>
+                    <img style={{marginBottom: 30}}
+                      src={t(`sections.${index}.image.src`)}
+                      alt={t(`sections.${index}.image.alt`)}
+                      width={"173px"}
+                    />
+                    <CardBody>
+                      <CardTitle>
+                        <h3>{t(`sections.${index}.title`)}</h3>
+                      </CardTitle>
+                    </CardBody>
+                  </Card>
+                </Link>
+              </Container>
+            ))
+            .filter(filterArticle)}
         </Row>
       </div>
       <Link href={t('button.href')}>
