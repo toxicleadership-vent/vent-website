3736e83ca66e777075a661bf6f3f47b2e02c2804
Eva Hoffmann <eva.hoffmann@trawa.de>
2024-06-22 19:28:18 +0200

    additional fix

--- src/app/[lang]/survey/page.tsx
+++ src/app/[lang]/survey/page.tsx
@@ -3,6 +3,7 @@ import { PageParams } from '../layout'
 import { Survey } from '@/components/survey/survey'
 import styles from './page.module.css'
 import { cookies } from 'next/headers'
+import rootStyles from '@/app/[lang]/rootStyles.module.css'
 
 export default async function Contact({ params }: { params: PageParams }) {
   const { t } = await getTranslation(params.lang, 'survey')
@@ -10,15 +11,19 @@ export default async function Contact({ params }: { params: PageParams }) {
   const isSurveyDone = cookieStore.get('survey')
 
   return (
-    <main className={styles.main}>
-      {!isSurveyDone ? (
-        <>
-          <h1 className={styles.title}>{t('title')}</h1>
-          <Survey lang={params.lang} />
-        </>
-      ) : (
-        <div>{t('done')}</div>
-      )}
+    <main className={`${rootStyles.section} ${styles.main}`}>
+      <div
+        className={`${rootStyles.sectionContainer} ${rootStyles.sectionContainerBottom}`}
+      >
+        {!isSurveyDone ? (
+          <>
+            <h1 className={styles.title}>{t('title')}</h1>
+            <Survey lang={params.lang} />
+          </>
+        ) : (
+          <div>{t('done')}</div>
+        )}
+      </div>
     </main>
   )
 }
