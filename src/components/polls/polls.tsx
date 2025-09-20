705483fed42dc610b61fe6d0bfd40d201ad6edd5
Judith Lippold <judithlippold@gmx.net>
2025-09-20 23:33:29 +0200

    adapt polls component to strapi api fetch

--- src/components/polls/polls.tsx
+++ src/components/polls/polls.tsx
@@ -7,26 +7,36 @@ import {
   Carousel,
   CarouselItem,
 } from '@/components/bootstrap/bootstrap'
-import { useTranslation } from '@/localization/i18n-client'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { PollQuestion } from './poll-question'
 import style from './polls.module.css'
 
 export const PollsContainer = ({ lang }: { lang: string }) => {
-  const { t } = useTranslation(lang, 'home', { keyPrefix: 'home' })
+  const [polls, setPolls] = useState<any[]>([])
   const [activeIndex, setActiveIndex] = useState(0)
+
+  useEffect(() => {
+    fetch(`https://typical-dogs-185f9ff416.strapiapp.com/api/home?locale=${lang}&populate[polls][populate]=*&populate[metadata][populate]=*`)
+      .then(res => res.json())
+      .then(json => {
+      setPolls(json.data.polls?.questions || [])
+    })
+  }, [lang])
+
+
   return (
     <Stack>
       <Row className={`space-between ${style.questionHeader}`}>
         <Col className={style.flexStart}>
-          {t('polls.question', { count: 4, no: activeIndex + 1 })}
+          {`Question ${activeIndex + 1}/${polls.length}`}
         </Col>
         <Col className={style.flexEnd}>
           <button
             className={style.button}
             onClick={() => setActiveIndex((activeIndex + 1) % 4)}
+            disabled={polls.length === 0}
           >
-            {t('polls.next')}
+            NEXT
             <Image src="/images/home/next-arrow.svg" alt="next" />
           </button>
         </Col>
