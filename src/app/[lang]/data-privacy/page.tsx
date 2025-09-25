d55021411920b2886365741de11758672c6df401
Judith Lippold <judithlippold@gmx.net>
2025-09-25 22:42:18 +0200

    take off i18n from data-provacy page

--- src/app/[lang]/data-privacy/page.tsx
+++ src/app/[lang]/data-privacy/page.tsx
@@ -1,4 +1,3 @@
-import { getTranslation } from '@/localization/i18n'
 import { PageParams } from '../layout'
 import { Stack } from '@/components/bootstrap/bootstrap'
 import styles from './page.module.css'
@@ -6,8 +5,7 @@ import PrivacyMdx from './data-privacty.mdx'
 import rootStyles from '../rootStyles.module.css'
 
 export default async function Imprint({ params }: { params: PageParams }) {
-  const { t } = await getTranslation(params.lang, 'general')
-
+  
   return (
     <main className={`${rootStyles.section} ${styles.main}`}>
       <div
