feb63adbb70ab43bee0e49330439efb8f8c98757
Judith Lippold <judithlippold@gmx.net>
2025-10-06 23:23:22 +0200

    get metatdata from strapi for data-privacy page layout

--- src/app/[lang]/data-privacy/layout.tsx
+++ src/app/[lang]/data-privacy/layout.tsx
@@ -2,40 +2,40 @@ import { Header } from '@/components/header/header'
 import { PageParams } from '../layout'
 import rootStyles from '../page.module.css'
 import styles from './page.module.css'
-import { getTranslation } from '@/localization/i18n'
 import { ResolvingMetadata, Metadata } from 'next'
+import meta from '@/components/survey/question.stories'
 
 export async function generateMetadata(
   { params }: { params: PageParams },
   parent: ResolvingMetadata
 ): Promise<Metadata> {
   const { lang } = params
-  const { t } = await getTranslation(lang, 'general', {
-    keyPrefix: 'general.metadata',
-  })
+  const { metadataCopy } = await fetch(`https://typical-dogs-185f9ff416.strapiapp.com/api/general?locale=${lang}&populate[metadata][populate]=*`);
+
+  const {data: metadata} = await metadataCopy.json();
 
   const robots = (await parent).robots?.basic
   const icons = (await parent).icons
   const keywords = (await parent).keywords
 
   return {
-    title: t('title'),
-    description: t('description'),
+    title: metadata.title,
+    description: metadata.description,
     robots,
     icons,
     openGraph: {
       type: 'website',
       url: 'https://www.toxicleadershipvent.com/data-privacy',
-      description: t('description'),
-      siteName: t('siteName'),
-      images: t('image'),
+      description: metadata.description,
+      siteName: metadata.siteName,
+      images: metadata.image,
     },
     twitter: {
       card: 'summary_large_image',
       site: '@evandyou',
-      images: { url: t('image'), alt: t('siteName') },
-      title: t('siteName'),
-      description: t('description'),
+      images: { url: metadata.image, alt: metadata.siteName },
+      title: metadata.siteName,
+      description: metadata.description,
     },
     keywords,
   }
